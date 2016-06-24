90ed06bd746cf0b3b44413d107f28fe57ac0da41
Karl Rohr <karlrohr@annihilation.fullsail.com>
2016-06-23 23:46:44 -0400

    Added enhancements

--- src/utool.js
+++ src/utool.js
@@ -1,6 +1,5 @@
-exports.debug = (title, obj) => {
+exports.debug = (title, obj, status) => {
   const colors = require('colors');
-  const fs = require('fs');
   const seperator = '\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n';
   const time = new Date();
   colors.setTheme({
@@ -12,10 +11,39 @@ exports.debug = (title, obj) => {
   });
   const output = seperator + title + '\n' + time + seperator + JSON.stringify(obj);
 
-  if (process.env.DEBUG) {
-    fs.appendFile('logs/eLog.log', output, 'utf8', (err) =>{
-      if (err) throw err;
-    });
-    console.log(output);
+  if (status != null) {
+    console.warn('Status is null');
+  } else {
+    console.log('Status: ' + status);
   }
+
+  if (process.env.DEBUG) {
+      console.log(output);
+    } else {
+      console.error(new Error('Environmental Variable DEBUG not true'));
+    }
+
+  }
+};
+
+exports.bump = (currentVersion, typeOfUpdate) => {
+  let hotfix = currentVersion.hotfix;
+  let minor = currentVersion.minor;
+  let major = currentVersion.major;
+  if (typeof typeOfUpdate) {
+    if (typeOfUpdate === 'hotfix') {
+      hotfix += 1;
+    }
+    if (typeOfUpdate === 'minor') {
+      minor += 1;
+    }
+    if (typeOfUpdate === 'major') {
+      hotfix = 0;
+      minor = 0;
+      major += 1;
+    }
+  } else {
+    console.warn('Please for your second argument please type in hotfix, minor, or major');
+  }
+  console.log('Your new version number should be: ' + major + '.' + minor + '.' + hotfix);
 };
